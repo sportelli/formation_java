e5bdfe828df8bf59d4192e8bca5a72f6e8c0ee93
felix <sportelli@gmail.com>
2018-02-21 14:44:16 +0100

    Ajout de commentaires

--- formation3/src/com/tswe/formation3/vue/Programme2.java
+++ formation3/src/com/tswe/formation3/vue/Programme2.java
@@ -9,6 +9,11 @@ import com.tswe.formation3.entities.Produit;
 import com.tswe.formation3.exceptions.PanierNonTrouveException;
 import com.tswe.formation3.services.FactureService;
 
+/**
+ * Cette classe permet de tester la création d'une facture
+ * @author felix
+ *
+ */
 public class Programme2 {
 
 	public static void main(String[] args){
@@ -17,15 +22,17 @@ public class Programme2 {
 			Panier panier = new Panier();
 			panier.setId(2);
 
+			// Ajout des "faux" produits
 			ArrayList<Produit> produits = new ArrayList<Produit>();
 			produits.add(new Produit(1, "crayon", 1.0));
 			produits.add(new Produit(2, "papier", 2.0));
 			panier.setProduits(produits);
 
+			// Ajout d'un "faux" client pour la facture
 			Client proprietaire = new Client();
 			proprietaire.setCodeClient("2123");
 			proprietaire.setId(1111);
-			proprietaire.setName("Felix SPORTELLI");
+			proprietaire.setName("SPORTELLI Felix");
 			panier.setProprietaire(proprietaire);
 			Facture f = new FactureService().creerFactureDepuisPanier(panier);
 			System.out.println(f);
