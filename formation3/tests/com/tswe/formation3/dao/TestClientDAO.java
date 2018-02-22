c83ed94ad12eed7fa98def347da021a723ad905c
felix <sportelli@gmail.com>
2018-02-22 19:43:22 +0100

    test

---
+++ formation3/tests/com/tswe/formation3/dao/TestClientDAO.java
@@ -0,0 +1,25 @@
+package com.tswe.formation3.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.tswe.formation3.exceptions.ClientNotFoundException;
+
+public class TestClientDAO {
+
+/*	@Test (expected=ClientNotFoundException.class )
+	public void testGetByIdNull() throws ClientNotFoundException{
+		new ClientDAO().getById(0);
+	}
+	
+	@Test (expected=ClientNotFoundException.class )
+	public void testGetByIdInconnu() throws ClientNotFoundException{
+		new ClientDAO().getById(9999999);
+	}
+
+	@Test
+	public void testGetByIdValide() throws ClientNotFoundException {
+		Assert.assertNotNull(new ClientDAO().getById(1));
+	}*/
+	
+}
